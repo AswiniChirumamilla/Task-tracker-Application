48fe694bcb5d4279930d49264d6a247e8be19e1b
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-30 10:53:14 +0530

    Create Login.js

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import API from '../services/api';
+
+export default function Login() {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleLogin = async () => {
+    const res = await API.post('/auth/login', form);
+    localStorage.setItem('token', res.data.token);
+  };
+
+  return (
+    <div>
+      <input onChange={e => setForm({...form, email: e.target.value})} />
+      <input type="password" onChange={e => setForm({...form, password: e.target.value})} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
