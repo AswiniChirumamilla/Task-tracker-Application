e4fbb1968a979519644666c7321dca31e77d708f
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-30 10:43:28 +0530

    Create task.js

---
+++ backend/routes/task.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const Task = require('../models/Task');
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+router.post('/', auth, async (req, res) => {
+  const task = new Task({ ...req.body });
+  await task.save();
+  res.status(201).json(task);
+});
+
+router.get('/:projectId', auth, async (req, res) => {
+  const tasks = await Task.find({ projectId: req.params.projectId });
+  res.json(tasks);
+});
+
+router.put('/:id', auth, async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+});
+
+router.delete('/:id', auth, async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.send('Deleted');
+});
+
+module.exports = router;
