b1544ca94c8d01e3f87ea3b960c5d5c170cd94e0
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-30 10:41:10 +0530

    Create auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const { name, email, password, country } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hashedPassword, country });
+  await user.save();
+  res.status(201).send('User created');
+});
+
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+    return res.status(401).send('Invalid credentials');
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+module.exports = router;
