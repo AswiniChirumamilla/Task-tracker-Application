7241021aba7242ff132f10fbeb8fa75cd008fddf
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-05-01 10:25:08 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) return res.status(403).send('Token missing');
