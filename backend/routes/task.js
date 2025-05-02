d9d90875a4fed6988b80183486652a402e2cf352
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-05-02 09:30:07 +0530

    Update task.js

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,7 +2,6 @@ const express = require('express');
 const Task = require('../models/Task');
 const auth = require('../middleware/auth');
 const router = express.Router();
-
 router.post('/', auth, async (req, res) => {
   const task = new Task({ ...req.body });
   await task.save();
